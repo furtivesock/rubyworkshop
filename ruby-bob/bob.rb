58439f10bc9671475e23f86b46666061b5aac1c0
Sofi <chaussettefurtive@outlook.fr>
2018-09-20 18:52:21 +0200

    Ajout de Bob

--- ruby-bob/bob.rb
+++ ruby-bob/bob.rb
@@ -1,11 +1,13 @@
 class Bob
     def hey(string)
-        if string == string.upcase
+        if string.downcase != string.upcase and string == string.upcase
             return "Whoa, chill out!"
         elsif string[-1] == "?"
             return "Sure."
-        else
+        elsif string.strip != ""
             return "Whatever."
+        else
+            return "Fine. Be that way!"
         end
     end
 end
